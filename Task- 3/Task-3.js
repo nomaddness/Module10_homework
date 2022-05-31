9a815c226de1e5d925f35e9ea6e17454de611853
Alexander <94503747+nomaddness@users.noreply.github.com>
2022-05-31 15:48:06 +0800

    Update Task-3.js

--- Task- 3/Task-3.js
+++ Task- 3/Task-3.js
@@ -10,42 +10,72 @@
 // и в чат вывести ссылку на https://www.openstreetmap.org/ с вашей гео-локацией. 
 // Сообщение, которое отправит обратно эхо-сервер, не выводить.
 
-const wsUrl = "wss://echo.websocket.org/";
+let input = document.querySelector('.j-input');
+let btnSend = document.querySelector('.j-btn-send');
+let btnLocation = document.querySelector('.j-btn-location');
+let chat = document.querySelector('.chat');
 
-function pageLoaded() {
-	const infoOutput = document.querySelector(".info_output");
-	const chatOutput = document.querySelector(".chat_output");
-	const input = document.querySelector("input");
-	const sendBtn = document.querySelector(".btn_send");
+const wsUrl = "wss://echo-ws-service.herokuapp.com";
 
-	let socket = new WebSocket(wsUrl);
+let websocket;
 
-	socket.onopen = () => {
-		infoOutput.innerText = "Соединение установлено";
-	}
-
-	socket.onmessage = (event) => {
-		writeToChat(event.data, true);
-	}
-
-	socket.onerror = () => {
-		infoOutput.innerText =  "При передаче данных произошла ошибка"
-	}
-
-	sendBtn.addEventListener('click', sendMessage); 
-
-	function sendMessage() {
-		if (!input.value) return;
-		//  или if (!input.value ==="") return;
-		socket.send(input.value);
-		writeToChat(input.value, false);
-		input.value === "";
-	}
-
-	function writeToChat(message, isRecieved) {
-		let messageHTML = `<div class="${isRecieved? "recieved" : "sent" }">${message}</div>`;
-		chatOutput.innerHTML += messageHTML;
-	}
+function writeToScreen(message, sender) {
+  let pre = document.createElement("p");
+  pre.className = `${sender}-message`;
+  pre.innerHTML = message;
+  pre.style.border = '1px solid blue';
+  pre.style.backgroundColor = 'lightblue';
+  pre.style.width = '100px';
+  pre.style.borderRadius = '5px';
+  if (sender == 'server') {
+       pre.style.backgroundColor = 'grey';
+       pre.style.alignSelf = 'flex-end';
+       pre.style.textAlign = 'right';
+       pre.style.margin = '5px';
+  }
+  chat.appendChild(pre);
 }
 
-document.addEventListener("DOMContentLoaded", pageLoaded) 
+if (!websocket) {
+  websocket = new WebSocket(wsUrl);
+}
+
+btnSend.addEventListener("click", () => {
+
+  websocket.onmessage = function (event) {
+    writeToScreen(event.data, "server");
+  };
+
+  websocket.onerror = function (event) {
+    writeToScreen(event.data, "server");
+  };
+
+  let input = document.querySelector('.j-input').value;
+    writeToScreen(input, "client");
+
+  websocket.send(input); 
+});
+
+btnLocation.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+      chat.textContent = "Geolocation не поддерживается вашим браузером";
+  } else {
+      chat.textContent = "Определение местоположения…";
+      navigator.geolocation.getCurrentPosition(success, error);
+      
+  }
+});
+
+const success = (position) => {
+  console.log("position", position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const message = `<a href="https://www.openstreetmap.org/#map=18/${latitude}/${longitude}" target="_blank">Ссылка на карту</a>`;
+  writeToScreen(message, 'client')
+};
+
+const error = () => {
+  chat.textContent = "Невозможно получить ваше местоположение";
+};
+
